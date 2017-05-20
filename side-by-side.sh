96f2e18a00e0dd2226378f391e4128e27139c5fa
Xuran Qian <xrqian@rcnx01.rc.fas.harvard.edu>
2017-05-20 11:06:31 -0400

    fix aspect ratio for videos

--- side-by-side.sh
+++ side-by-side.sh
@@ -1,11 +1,11 @@
-load module ffmpeg
-ffmpeg -r 2 -i dust-map-left.%05d.png dust-map-left.mp4
-ffmpeg -r 2 -i dust-map-right.%05d.png dust-map-right.mp4
+module load ffmpeg
+ffmpeg -r 2 -i dust-map-cl-left.%05d.png dust-map-left.mp4
+ffmpeg -r 2 -i dust-map-cl-right.%05d.png dust-map-right.mp4
 ffmpeg \
   -i dust-map-left.mp4 \
   -i dust-map-right.mp4 \
-  -filter_complex '[0:v]pad=iw*2:ih[int];[int][1:v]overlay=W/2:0[vid]' \
-  -map [vid] \
+  -filter_complex '[0:v]scale=iw:ih,setsar=sar/2,pad=2*iw:ih [left];[1:v]scale=iw:ih, setsar=sar/2[right];[left][right]overlay=main_w/2:0 [out]' \
+  -map [out] \
   -c:v libx264 \
   -crf 23 \
   -preset veryfast \
